6fbb2930d6dd11f588906daa8411b9d1eb4ee799
Wajeehasiddiqui <124240689+Wajeehasiddiqui@users.noreply.github.com>
2023-02-12 19:48:41 +0500

    Add files via upload

---
+++ Program#4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "I Wandered Lonely as a Cloud\n\n"
+         << "I wandered lonely as a cloud\n"
+         << "That floats on high o'er vales and hills,\n"
+         << "When all at once I saw a crowd,\n"
+         << "A host of golden daffodils;\n"
+         << "Beside the lake, beneath the trees,\n"
+         << "Fluttering and dancing in the breeze.\n\n"
+         << "Continuous as the stars that shine\n"
+         << "And twinkle on the Milky Way,\n"
+         << "They stretched in never-ending line\n"
+         << "Along the margin of a bay:\n"
+         << "Ten thousand saw I at a glance,\n"
+         << "Tossing their heads in sprightly dance.\n\n"
+         << "The waves beside them danced, but they\n"
+         << "Out-did the sparkling waves in glee:\n"
+         << "A poet could not but be gay,\n"
+         << "In such a jocund company:\n"
+         << "I gazed—and gazed—but little thought\n"
+         << "What wealth the show to me had brought:\n\n"
+         << "For oft, when on my mind\n"
+         << "The burden of the barren hours had fallen,\n"
+         << "That inward eye which is the bliss of solitude,\n"
+         << "And is its heritage, had brought me\n"
+         << "An Endless Store of Memories\n"
+         << "Of golden daffodils.\n";
+    return 0;
+}
